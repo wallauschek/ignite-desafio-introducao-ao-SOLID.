bc768fab825345a656878e4ceb15f4bde6cf36f1
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2021-03-21 10:26:32 -0300

    correção de tipagem na request.headers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -10,10 +10,7 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
-    if (Array.isArray(user_id)) {
-      throw Error("user_id must be a string");
-    }
+    const { user_id } = <{ user_id: string }>request.headers;
     try {
       const users = this.listAllUsersUseCase.execute({ user_id });
 
